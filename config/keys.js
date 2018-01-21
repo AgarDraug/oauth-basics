d0bf132b1c09cabeb902a03241ebccc990d80fd6
AgarDraug <andre.2107.silva@gmail.com>
2018-01-21 18:08:04 +0000

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+// add this to .gitignore
+
+module.exports = {
+  google: {
+    clientID: '141525832319-rccq1d5p4s26ddl001dsnv18jqombpfr.apps.googleusercontent.com',
+    clientSecret: 'Pj0dTA1NKe03XwnOt9SEsmHp'
+  },
+  mongodb: {
+    dbURI: 'mongodb://oauth-admin:test@ds111078.mlab.com:11078/oauth-basics'
+  },
+  session: {
+    cookieKey: 'liahjsdbgfliausbfliakuejbfaslkdjfbasfbc'
+  }
+};
